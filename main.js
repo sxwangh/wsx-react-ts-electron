ee39093e551b0077e080014bdb533a82b1a856da
wangsixue <wangsixue@baidu.com>
2023-04-21 15:47:42 +0800

    feature(): electron-builder打包dmg--success

--- main.js
+++ main.js
@@ -1,24 +1,32 @@
 // Modules to control application life and create native browser window
 
 const {app, BrowserWindow} = require('electron')
-const path = require('path')
+// const path = require('path')
+const pkg = require('./package.json') // 引用package.json
+// const url = require("url");
 
 function createWindow () {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
-    // webPreferences: {
+    webPreferences: {
+        // webSecurity: false,
+        nodeIntegration: true,
       // preload: path.join(__dirname, 'preload.js')
-    // }
+    }
   })
 
   // and load the index.html of the app.
-  // mainWindow.loadFile('index.html')
-  mainWindow.loadURL('http://localhost:3000/');
+  if (pkg.DEV) {
+    mainWindow.loadURL("http://localhost:3000/")
+  } else {
+    mainWindow.loadFile('./build/index.html')
+  }
+  window.webContents.openDevTools();
 
   // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+  mainWindow.webContents.openDevTools({mode:'bottom'});
 }
 
 // This method will be called when Electron has finished
@@ -30,7 +38,6 @@ app.whenReady().then(() => {
   app.on('activate', function () {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
 })
 
